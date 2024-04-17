b67fcfef221f718bae2f6c4fede520e0d1ed2eb3
juliorafaelc <142450645+juliorafaelc@users.noreply.github.com>
2024-04-17 17:07:33 -0300

    adicionado função de criar usuario

--- migracao_api/usuarios/consulta_usuarios.rb
+++ migracao_api/usuarios/consulta_usuarios.rb
@@ -6,9 +6,21 @@ class Usuarios
 
   def initialize
     json_content = File.read('../migracao_api/usuarios/BD.json')
+    # json_content = File.read('BD.json') teste local durante desenvolvimento
     @usuarios = {}
     @usuarios['code'] = 200
     @usuarios['response'] = JSON.parse(json_content)
+  end
+
+  def criar_usuario(nome, email, cargo)
+    id = criar_proximo_id(@usuarios['response'])
+    @usuarios['response'] << { 'id' => id, 'nome' => nome, 'email' => email, 'cargo' => cargo }
+    # File.write('./BD.json', JSON.pretty_generate(@usuarios['response'])) teste local durante desenvolvimento
+    File.write('../migracao_api/usuarios/BD.json', JSON.pretty_generate(@usuarios['response']))
+  end
+
+  def criar_proximo_id(response)
+    return response.last['id'] + 1
   end
 
   def consultar_todos
@@ -47,6 +59,6 @@ class Usuarios
 end
 
 # Como usar:
-teste = Usuarios.new.consultar_todos
-teste = Usuarios.new.consultar_por_id(1)
-
+#teste = Usuarios.new.consultar_todos
+#teste = Usuarios.new.consultar_por_id(1)
+#teste = Usuarios.new.criar_usuario('teste', 'teste@gmail.com', 'Tester')
